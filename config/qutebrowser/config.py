5678fabb4f23725f5cb93be8708c92f7d92a218b
Mike Bannister <notimpossiblemike@gmail.com>
2022-06-02 21:57:46 -0400

    Update qute settings

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,15 +1,20 @@
 config.load_autoconfig()
 
-c.url.searchengines = {
-    'DEFAULT':  'https://google.com/search?hl=en&q={}'
-}
+c.url.searchengines = {"DEFAULT": "https://google.com/search?hl=en&q={}"}
 
-config.bind('K', 'tab-next')
-config.bind('J', 'tab-prev')
+config.bind("K", "tab-next")
+config.bind("J", "tab-prev")
 
-config.set("colors.webpage.bg", "#1c1c1c")
+config.bind("<<", "tab-move -")
+config.bind(">>", "tab-move +")
+
 config.set("tabs.show", "multiple")
 config.set("statusbar.show", "in-mode")
+
+config.set("window.hide_decoration", True)
+config.set("colors.webpage.bg", "#1c1c1c")
+
+config.set("auto_save.session", True)
 
 c.url.default_page = "about:blank"
 c.url.start_pages = ["about:blank"]
