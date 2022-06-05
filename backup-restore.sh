cae2bd64b763c0e46cb1e011a5cc634a61a6d2d6
Mike Bannister <notimpossiblemike@gmail.com>
2022-06-06 00:13:59 +0200

    Restore ssh key and gist configs

--- backup-restore.sh
+++ backup-restore.sh
@@ -7,16 +7,42 @@ if [ -z "${BACKUP_NAME}" ]; then
   exit 1
 fi
 
-# * Ssh key
 # * Zsh history
-# * Gist creds
-# * Gist-vim creds
 # * Slack creds
 # * Browser meta
 # * Lab creds
 
 duplicity \
+  --force \
   --file-to-restore \
-  home/mike/.private_environment \
+  .private_environment \
   "s3:///mike-backups-4c256a80-e412-11ec-94cf-5f96b9da8566/${BACKUP_NAME}" \
   /home/mike/.private_environment
+
+duplicity \
+  --force \
+  --file-to-restore \
+  .ssh/id_ed25519 \
+  "s3:///mike-backups-4c256a80-e412-11ec-94cf-5f96b9da8566/${BACKUP_NAME}" \
+  /home/mike/.ssh/id_ed25519
+
+duplicity \
+  --force \
+  --file-to-restore \
+  .ssh/id_ed25519.pub \
+  "s3:///mike-backups-4c256a80-e412-11ec-94cf-5f96b9da8566/${BACKUP_NAME}" \
+  /home/mike/.ssh/id_ed25519.pub
+
+duplicity \
+  --force \
+  --file-to-restore \
+  .gist \
+  "s3:///mike-backups-4c256a80-e412-11ec-94cf-5f96b9da8566/${BACKUP_NAME}" \
+  /home/mike/.gist
+
+duplicity \
+  --force \
+  --file-to-restore \
+  .gist-vim \
+  "s3:///mike-backups-4c256a80-e412-11ec-94cf-5f96b9da8566/${BACKUP_NAME}" \
+  /home/mike/.gist-vim
