32d33b887f1a7d062726d81993009c24a4ca0e08
Mike Bannister <notimpossiblemike@gmail.com>
2022-06-06 22:27:45 -0400

    Remove some overly agressive cleanup after bootstrap

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -321,41 +321,6 @@ echo "clear the history so our install isn't there"
 sudo rm -f /root/.wget-hsts
 export HISTSIZE=0
 
-echo "whiteout root"
-
-count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
-count=$(($count-1))
-sudo dd if=/dev/zero of=/tmp/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
-sudo rm /tmp/whitespace
-
-echo "whiteout /boot"
-
-count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
-count=$(($count-1))
-sudo dd if=/dev/zero of=/boot/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
-sudo rm /boot/whitespace
-
-echo "cleanup misc"
-
-set +e
-swapuuid="`/sbin/blkid -o value -l -s UUID -t TYPE=swap`";
-case "$?" in
-    2|0) ;;
-    *) exit 1 ;;
-esac
-set -e
-
-if [ "x${swapuuid}" != "x" ]; then
-    # Whiteout the swap partition to reduce box size
-    # Swap is disabled till reboot
-    swappart="`readlink -f /dev/disk/by-uuid/$swapuuid`";
-    /sbin/swapoff "$swappart" || true;
-    dd if=/dev/zero of="$swappart" bs=1M || echo "dd exit code $? is suppressed";
-    /sbin/mkswap -U "$swapuuid" "$swappart";
-fi
-
-sync;
-
 echo "bootstrap dotfiles"
 
 if [ ! -d "/home/mike/code/dotfiles" ]; then
