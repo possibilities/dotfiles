acb057b29af51c77514423b6b5f80ebf9d70ccda
Mike Bannister <notimpossiblemike@gmail.com>
2023-04-05 11:20:17 -0400

    Add loopback for obs virtual cam

--- bootstrap-system.sh
+++ bootstrap-system.sh
@@ -39,6 +39,7 @@ sudo apt install --yes \
   p7zip-full
 
 echo "setup modprobe for obs virtual camera"
+sudo apt install --yes v4l2loopback-dkms
 echo v4l2loopback | sudo tee /etc/modules-load.d/v4l2loopback.conf
 
 echo install btm
